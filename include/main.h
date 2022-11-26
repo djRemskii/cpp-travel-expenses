63432e04cf5199e7390c878e3f8f72c1ddca3dac
djRemskii <jeremyong@cpp.edu>
2022-11-26 15:32:37 -0800

    added all variables and methods to be implemented, as well as a few notes

--- include/main.h
+++ include/main.h
@@ -1,16 +1,33 @@
 /* 
+ * FILE: main.h
+ * AUTHOR: Remi/Jeremy Ong 
+ *
  * primary structure file for program, outlines all the variables and methods
  * that will be used by the program.
  */
 
+//Variables to get input for
 int numberOfDays;
 int departTime;
 int returnTime;
 double airfare;
 double carRentalCost;
 int milesDriven;
-double[] parkingFees;
-double[] taxiFees;
+double parkingFees[];
+double taxiFees[];
 double registrationFees;
 double hotelCost;
-double[][] mealCosts;
+double mealCosts[][3];
+
+//Variables to be calculated and output
+double totalExpenses;
+double totalAllowable;
+double reimburseAmount;
+double savedAmount;
+
+//Calculation methods
+void calculateTotalExpenses();
+void calculateTotalAllowable();
+void calculateReimburseAmount();
+void calculateSavedAmount();
+
