5d0f05e91c211a72eaf6fd0c4fdf49114506b821
djRemskii <jeremyong@cpp.edu>
2022-11-26 15:19:08 -0800

    Created main.h in /include

---
+++ include/main.h
@@ -0,0 +1,16 @@
+/* 
+ * primary structure file for program, outlines all the variables and methods
+ * that will be used by the program.
+ */
+
+int numberOfDays;
+int departTime;
+int returnTime;
+double airfare;
+double carRentalCost;
+int milesDriven;
+double[] parkingFees;
+double[] taxiFees;
+double registrationFees;
+double hotelCost;
+double[][] mealCosts;
