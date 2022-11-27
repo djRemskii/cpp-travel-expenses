d8236af45e7f769a4e7dbe9c89d25b2c36ccfe27
djRemskii <jeremyong@cpp.edu>
2022-11-26 16:19:15 -0800

    created main.c, with main method and plan to implment other files. past commit did not include the actual file lol. this one does.

---
+++ src/main.c
@@ -0,0 +1,33 @@
+/* 
+ * FILE: main.c
+ * AUTHOR: Remi/Jeremy Ong 
+ *
+ * file to hold all the variables and methods that will be used by the program.
+ */
+
+#include "main.h"
+
+int numberOfDays;
+int departTime;
+int returnTime;
+double airfare;
+double carRentalCost;
+int milesDriven;
+double parkingFees[];
+double taxiFees[];
+double registrationFees;
+double hotelCost;
+double mealCosts[][3];
+
+void main(void){
+    //Print introduction to program to terminal
+
+
+    //Prompt for user input to assign values to each variable
+
+
+    //After all variables have values, calculate 
+
+
+    //Output to user
+}
